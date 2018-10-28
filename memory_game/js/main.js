4963aa3f82ceaebf37ee5a1830061985c0433fb8
May Alluqmani <maymo7a@Mays-MacBook-Pro.local>
2018-10-28 21:56:50 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,30 @@
 //console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+//var cardOne = "queen";
+//var cardTwo = "king";
+//var cardThree = "queen";
+//var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardTwo);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = [0];
+
+ var cardsInPlay = [];
+ cardsInPlay.push('cardOne');
+ cardsInPlay;
+ console.log('User flipped queen');
+
+ var cardTwo = [2];
+ cardsInPlay.push('cardTwo');
+ cardsInPlay;
+ console.log('User flipped king');
+
+
+if (cardsInPlay[0] === cardsInPlay[2]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
